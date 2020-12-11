c2ab4a0054b8baf26a38d89393c42ffd353f6aba
Sunny <sunnycheungabc@gmail.com>
2020-12-11 10:45:41 +0800

    Switched to gevent as web server

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from server import app
+from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    http_server = WSGIServer(('0.0.0.0', 5000), app.wsgi_app)
+    http_server.serve_forever()
