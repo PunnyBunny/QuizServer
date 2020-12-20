67fa2e19ef5b5c18f68ddbe362320c990e038d76
Sunny <sunnycheungabc@gmail.com>
2020-12-20 13:37:18 +0800

    Add messages

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from server import app
 from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
+    print("Server starting...")
     http_server = WSGIServer(('0.0.0.0', 5000), app.wsgi_app)
+    print("Server running...")
     http_server.serve_forever()
