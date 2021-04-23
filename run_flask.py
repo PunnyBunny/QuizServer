838d1c5f3643989f2f9526cfa46a041cf9bd0966
Sunny Cheung <sunnycheungabc@gmail.com>
2021-04-23 14:40:12 +0800

    https

--- run_flask.py
+++ run_flask.py
@@ -2,5 +2,5 @@ from server import app
 import sys
 if __name__ == '__main__':
     print("Server running...")
-    app.run('0.0.0.0', int(sys.argv[1]))
+    app.run('0.0.0.0', int(sys.argv[1]), ssl_context='adhoc')
 
