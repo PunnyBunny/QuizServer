9127474823916f79d8d64ebebcd7dfe765eee1a7
Sunny Cheung <sunnycheungabc@gmail.com>
2021-04-23 20:38:17 +0800

    hacky ssl

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ from server import app
 
 
 def main():
-    print("Server starting...")
     if len(sys.argv) != 2:
         print("Usage: python run.py [port number]")
         return
     port = int(sys.argv[1])
-    http_server = WSGIServer(('0.0.0.0', port), app.wsgi_app, ssl_context='adhoc')
+    print("Server starting...")
+    http_server = WSGIServer(('0.0.0.0', port), app.wsgi_app)
     print("Server running...")
     http_server.serve_forever()
 
--- run_flask.py
+++ run_flask.py
@@ -1,6 +1,17 @@
-from server import app
 import sys
-if __name__ == '__main__':
-    print("Server running...")
-    app.run('0.0.0.0', int(sys.argv[1]), ssl_context='adhoc')
 
+from server import app
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python run_flask.py [port number]")
+        return
+    print("Server starting...")
+    port = int(sys.argv[1])
+    print('Server running...')
+    app.run(('0.0.0.0', port), ssl_context='adhoc')
+
+
+if __name__ == '__main__':
+    main()
