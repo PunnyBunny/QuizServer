f8e74cce247f1546c9ef59c9ab2dfe0df0cebd33
Sunny Cheung <sunnycheungabc@gmail.com>
2021-04-23 20:53:55 +0800

    cring e

--- server/routes.py
+++ server/routes.py
@@ -5,24 +5,31 @@ from flask import request
 
 from server import app, actions, models, db
 
-"""
-type: 'audio' or 'mc'
-name: name of participant
-date_of_birth: date of birth of participant in DD-MM-YYYY format
-gender: gender of participant ('m' or 'f')
-school_name: name of school of participant
-grade_level: grade level of participant
 
-test_name: name of the test
-full_score: length of the test
-
-audios: if type is 'audio' then an audios.zip containing the audio of the participant
-
-score: if type is 'mc' then score of participant in the test
-"""
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    print('here')
+    """
+    type: 'audio' or 'mc'
+    name: name of participant
+    date_of_birth: date of birth of participant in DD-MM-YYYY format
+    gender: gender of participant ('m' or 'f')
+    school_name: name of school of participant
+    grade_level: grade level of participant
+
+    test_name: name of the test
+    full_score: length of the test
+
+    audios: if type is 'audio' then an audios.zip containing the audio of the participant
+
+    score: if type is 'mc' then score of participant in the test
+    """
+    if request.method == 'GET':
+        return '''
+            <!doctype html>
+            <title>JOON GAY</title>
+            <h1>JOON GAY</h1>
+        '''
+
     type_ = request.form['type']
     name = request.form['name'].strip()
     date_of_birth = datetime.strptime(request.form['date_of_birth'], '%d-%m-%Y').date()
@@ -33,11 +40,14 @@ def home():
     test_name = request.form['test_name']
     length = int(request.form['length'])
 
-    person = models.Person.query.filter_by(name=name, date_of_birth=date_of_birth, gender=gender, school_name=school_name, grade_level=grade_level).first()
+    person = models.Person.query.filter_by(name=name, date_of_birth=date_of_birth, gender=gender,
+                                           school_name=school_name, grade_level=grade_level).first()
     if not person:
-        person = models.Person(name=name, date_of_birth=date_of_birth, gender=gender, school_name=school_name, grade_level=grade_level)
+        person = models.Person(name=name, date_of_birth=date_of_birth, gender=gender, school_name=school_name,
+                               grade_level=grade_level)
         db.session.add(person)
-        person = models.Person.query.filter_by(name=name, date_of_birth=date_of_birth, gender=gender, school_name=school_name, grade_level=grade_level).first()
+        person = models.Person.query.filter_by(name=name, date_of_birth=date_of_birth, gender=gender,
+                                               school_name=school_name, grade_level=grade_level).first()
     test = models.Test.query.filter_by(name=test_name, length=length, type_=type_).first()
     if not test:
         test = models.Test(name=test_name, length=length, type_=type_)
